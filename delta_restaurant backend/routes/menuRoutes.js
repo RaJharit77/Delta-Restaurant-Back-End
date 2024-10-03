e9ab2f19b0f962c59ba3493710b088181fe6955c
RaJharit77 <hei.rajo.2@gmail.com>
2024-10-03 23:34:56 +0300

    feat: changing all of names

--- delta_restaurant backend/routes/menuRoutes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { getMenu } from '../controllers/menuController.js';
-
-const router = Router();
-
-router.get('/', getMenu);
-
-export default router;
--- delta_restaurant backend/server.js
+++
@@ -1,7 +0,0 @@
-import app from './app.js';
-
-const port = process.env.PORT || 5000;
-
-app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-});
