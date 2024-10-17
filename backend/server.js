47417b066972e2e13ed3f6aad126bba9cd7636ad
RaJharit77 <hei.rajo.2@gmail.com>
2024-10-17 23:34:19 +0300

    feat: updating server.js and vercel.json

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,8 @@ const PORT = process.env.PORT || 5000;
 app.use(cors({
     origin: 'https://delta-restaurant-madagascar.vercel.app',  
     methods: 'GET,POST,PUT,DELETE',
-    allowedHeaders: 'Content-Type,Authorization'
+    allowedHeaders: 'Content-Type,Authorization',
+    credentials: true
 }));
 
 app.use(cors());
